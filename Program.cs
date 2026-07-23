61f5060ab52f3be0beeb24c9be729e16c48fcf1f
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-07-22 20:59:08 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=======DATOS ACADEMICOS========");
+        Console.WriteLine("Nombre: Fatima Jimenez\n" + "Grado: IVC - Clave:15\n");
+        Console.Write("Introduce una frase: ");
+        string frase = Console.ReadLine();
+        string fraseInvertida = "";
+        for (int i = frase.Length - 1; i >= 0; i--)
+        {
+            fraseInvertida += frase[i];
+        }
+        Console.WriteLine("\nLa frase invertida es:");
+        Console.WriteLine(fraseInvertida);
+    }
+}
